c28c784fe59fdabb9de7852711add33e83f34fae
Stephanie <sbowl001@fiu.edu>
2018-04-10 12:21:18 +0800

    answers

--- client/src/components/FriendsList.js
+++ client/src/components/FriendsList.js
@@ -2,6 +2,23 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class FriendsList extends Component {
+  constructor(props){
+    super(props);
+    this.state = { friends: [] 
+    }
+  }
+  
+
+  componentDidMount(){
+    axios
+    .get("http://localhost:5000/friends")
+    .then(response => {
+      this.setState( ()=> ({friends:response.data}));
+    } )
+    .catch((error) => {
+      console.error("error", error);
+    })
+  }
   render() {
     return (
       <div>
